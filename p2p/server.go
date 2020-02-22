5177a5c89f001a3aedf9cb603674e40b48977a17
oigele <raphael.beckmann@yahoo.de>
2020-02-22 15:16:46 +0100

    one more statement

--- p2p/server.go
+++ p2p/server.go
@@ -191,6 +191,7 @@ func peerConn(p *peer) {
 	go peerBroadcast(p)
 
 	for {
+		logger.Printf("Before receiving data. Peer: %s")
 		header, payload, err := RcvData(p)
 		logger.Printf("Received message from %s", p.getIPPort())
 		if err != nil {
